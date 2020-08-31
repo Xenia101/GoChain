8161440a6c22b2398514bad51811a38082a23c83
Xenia <lommy101@gmail.com>
2020-09-01 01:25:33 +0900

    Add files via Upload

--- 1. prototype/main.go
+++ 1. prototype/main.go
@@ -8,20 +8,94 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
+	"math"
+	"math/big"
 	"strconv"
 	"time"
 )
+
+// 난이도
+const targetBits = 24
 
 // 배열을 활용하여 블록체인 구조 구현
 type Blcokchain struct {
 	blocks []*Block
 }
 
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+// 블록 구성
 type Block struct {
 	Timestamp     int64  // 블록 생성 시간
 	Data          []byte // 블록에 포한된 정보
 	PrevBlockHash []byte // 이전 블록의 해시값
 	Hash          []byte // 해당 블록의 해시값
+	Nonce         int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits)) // 256 - targetBits Left Shift
+	// target : 0x10000000000000000000000000000000000000000000000000000000000
+
+	pow := &ProofOfWork{b, target}
+	return pow
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+
+	for nonce < math.MaxInt64 {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+
+		hashInt.SetBytes(hash[:])
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
+}
+
+func IntToHex(n int64) []byte {
+	return []byte(strconv.FormatInt(n, 16))
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+	return data
+}
+
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+	return isValid
 }
 
 // 블록을 구성하는 필드들을 하나로 이은 뒤 SHA-256으로 해시화
@@ -34,8 +108,13 @@ func (b *Block) SetHash() {
 
 // 블록 생성
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	// block.SetHash()
+	block.Hash = hash[:]
+	block.Nonce = nonce
 	return block
 }
 
@@ -65,6 +144,10 @@ func main() {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n\n", block.Hash)
+
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
 
